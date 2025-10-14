ff700b09528d5474be927f68b57bf7870fbed15b
Clint Yarwood <chanandler@me.com>
2025-10-14 21:24:48 +0100

    Initial Commit

---
+++ Bandwidth-Monitor/Bandwidth_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bandwidth_MonitorApp.swift
+//  Bandwidth-Monitor
+//
+//  Created by Clint Yarwood on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Bandwidth_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
