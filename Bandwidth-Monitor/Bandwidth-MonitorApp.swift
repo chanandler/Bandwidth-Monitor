1a1f88b677583de77841dbb12f41142b1a689078
Clint Yarwood <chanandler@me.com>
2025-11-01 15:17:14 +0000

    Update Bandwidth-MonitorApp.swift

--- Bandwidth-Monitor/Bandwidth-MonitorApp.swift
+++ Bandwidth-Monitor/Bandwidth-MonitorApp.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Clint Yarwood on 15/10/2025
 //  With help from lots of coffee
+//  And kitty cat cuddles
 //
 
 // Entry point has moved to MenuBarBandwidthMonitorApp in ContentView.swift
